e09c63bb1cfe7f66c87cb009d51121eeaf25d395
CodeWithMourad <mourad.benammar@outlook.com>
2024-10-24 20:56:57 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import {
+  CallControls,
+  CallingState,
+  SpeakerLayout,
+  StreamCall,
+  StreamTheme,
+  StreamVideo,
+  StreamVideoClient,
+  useCallStateHooks,
+  User,
+} from '@stream-io/video-react-sdk';
+
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+
+const apiKey = 'Your_Api_Key';
+const token = 'YourToken';
+const userId = 'Your_userId';
+const callId = 'Your_callId';
+
+const user: User = {
+  id: userId,
+  name: 'M0BEAM',
+  image: 'https://getstream.io/random_svg/?id=M0BEAM&name=M0BEAM',
+};
+
+const client = new StreamVideoClient({ apiKey, user, token });
+const call = client.call('default', callId);
+call.join({ create: true });
+
+export default function App() {
+  return (
+    <StreamVideo client={client}>
+      <StreamCall call={call}>
+        <MyUILayout />
+      </StreamCall>
+    </StreamVideo>
+  );
+}
+
+export const MyUILayout = () => {
+  const { useCallCallingState } = useCallStateHooks();
+  const callingState = useCallCallingState();
+
+  if (callingState !== CallingState.JOINED) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <StreamTheme>
+      <SpeakerLayout participantsBarPosition='bottom' />
+      <CallControls />
+    </StreamTheme>
+  );
+};
---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const STREAM_API_KEY = 'your_stream_api_key';
+export const USER_ID = 'your_user_id';
+export const USER_TOKEN = 'your_user_token';
